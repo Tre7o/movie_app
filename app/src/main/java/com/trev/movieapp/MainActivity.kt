065fc5c8d97411e706934ff9c2376034e5facea5
John Trevor <softwarerecess09@gmail.com>
2023-09-23 14:49:25 +0300

    Added Credentials.kt, updated dependencies and project

--- app/src/main/java/com/trev/movieapp/MainActivity.kt
+++ app/src/main/java/com/trev/movieapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.trev.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.net.Uri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ATTENTION: This was auto-generated to handle app links.
+        val appLinkIntent: Intent = intent
+        val appLinkAction: String? = appLinkIntent.action
+        val appLinkData: Uri? = appLinkIntent.data
+        println(appLinkData)
     }
 }
